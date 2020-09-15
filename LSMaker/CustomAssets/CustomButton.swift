b58b488b20c78ef27bdac76674015848f9affc64
Alex Lopez <cristianalex.lopez@students.salle.url.edu>
2020-09-15 08:04:17 -0700

    added: button click animation

---
+++ LSMaker/CustomAssets/CustomButton.swift
@@ -0,0 +1,52 @@
+//
+//  CustomButton.swift
+//  LSMaker
+//
+//  Created by Alex Lopez on 15/09/2020.
+//  Copyright © 2020 Alex Lopez. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setConfig() {
+        backgroundColor = UIColor.green.withAlphaComponent(0.7)
+        setTitleColor(.white, for: .normal)
+        layer.borderColor = CGColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+        layer.borderWidth = 1
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    // Button onClick animation
+    func touchIn() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.allowUserInteraction, .curveEaseIn], animations: {
+            self.transform = .init(scaleX: 0.9, y: 0.9)
+        }, completion: nil)
+    }
+    
+    func touchEnd() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.allowUserInteraction, .curveEaseOut], animations: {
+            self.transform = .identity
+        }, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        touchIn()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        touchEnd()
+    }
+}
