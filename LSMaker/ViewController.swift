f0cc34fbe9d220aa1b956abf3f5ae962bc596e08
Alex Lopez <cristianalex.lopez@students.salle.url.edu>
2020-09-18 06:50:08 -0700

    added: first view

---
+++ LSMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LSMaker
+//
+//  Created by Alex Lopez on 04/08/2020.
+//  Copyright © 2020 Alex Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
